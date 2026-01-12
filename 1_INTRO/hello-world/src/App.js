5ea8e09120a8e6cbc8e0baaacfec63dbea34cce3
BiSousa <biacsousa72@gmail.com>
2026-01-12 16:46:08 -0300

    feat: initialize hello-world React app with essential files and configurations
    - Added package.json with dependencies and scripts for React app
    - Created public assets: favicon.ico, index.html, logo192.png, logo512.png, manifest.json, and robots.txt
    - Implemented basic App component with styling in App.css
    - Set up index.js for rendering the App component and reportWebVitals
    - Included testing setup with App.test.js and setupTests.js

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <h2>Welcome to Bia's React App</h2>
+    </div>
+  );
+}
+
+export default App;
