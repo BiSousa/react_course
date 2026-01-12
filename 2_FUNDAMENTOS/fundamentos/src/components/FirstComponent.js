ebface21eb8c696a2a5b7e7f92b2f207f95659cf
BiSousa <biacsousa72@gmail.com>
2026-01-12 17:05:01 -0300

    feat: initialize hello-world React app with essential files and configurations
    - Added package.json with dependencies and scripts for React app
    - Created public assets: favicon.ico, index.html, logo192.png, logo512.png, manifest.json, and robots.txt
    - Implemented basic App component with styling in App.css
    - Set up index.js for rendering the App component and reportWebVitals
    - Included testing setup with App.test.js and setupTests.js

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,18 @@
+// arquivo de estilo
+
+const FirstComponent = () => {
+  // essa função faz isso
+
+  /*
+    multi line
+  */
+
+  return (
+    <div>
+      {/* Algum comentário */}
+      <h1>Meu primeiro Componente</h1>
+    </div>
+  );
+};
+
+export default FirstComponent;
