5697d8a8051c2e84c5ad10bca20a3581a8d2cd9f
BiSousa <biacsousa72@gmail.com>
2026-01-16 10:39:12 -0300

    feat: implement MyForm component with basic form handling and styling

---
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./MyForm.css";
+
+const MyForm = () => {
+  // 3 - gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  console.log(name);
+  console.log(email);
+
+  return (
+    <div>
+      {/* 1 - criação de form */}
+      <form>
+        <div>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+          />
+        </div>
+        {/* 2 - label envolvendo input */}
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
+    </div>
+  );
+};
+
+export default MyForm;
