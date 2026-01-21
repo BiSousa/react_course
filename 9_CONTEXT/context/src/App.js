5462a6b653263741b03b830976978cd4bca9cbc9
BiSousa <biacsousa72@gmail.com>
2026-01-21 11:42:13 -0300

    feat: add context initial project

---
+++ 9_CONTEXT/context/src/App.js
@@ -0,0 +1,25 @@
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Context API</h1>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/" element={<Products />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
