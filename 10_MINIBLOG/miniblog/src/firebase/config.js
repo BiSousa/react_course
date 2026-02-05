4fe982df236d3972925113018165c1a7d64c1ada
Beatriz Carvalho Sousa <151962990+BiSousa@users.noreply.github.com>
2026-02-05 08:16:16 -0300

    Update config.js

--- 10_MINIBLOG/miniblog/src/firebase/config.js
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1GmD9DHPkcjlG_JsAJcqsGClPWrONUTQ",
-  authDomain: "miniblog-4cad6.firebaseapp.com",
-  projectId: "miniblog-4cad6",
-  storageBucket: "miniblog-4cad6.firebasestorage.app",
-  messagingSenderId: "1076548525018",
-  appId: "1:1076548525018:web:0a4affb66b09ee034bbee3",
+  // firebase config
 };
 
 const app = initializeApp(firebaseConfig);
